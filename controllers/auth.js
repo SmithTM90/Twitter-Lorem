7ab4c193f520e3cc29c28e9c9b29ef1933d17e5a
Travis Smith <smithtm90@gmail.com>
2016-06-24 13:50:17 -0700

    set up heroku config

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var db = require('../models');
+var passport = require('../config/ppConfig');
 var router = express.Router();
 
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
 });
 
-router.post('/signup', function(req,res) {
+router.post('/signup', function(req, res) {
   db.user.findOrCreate({
     where: { email: req.body.email },
     defaults: {
@@ -15,8 +16,10 @@ router.post('/signup', function(req,res) {
     }
   }).spread(function(user, created) {
     if (created) {
-      console.log('user created!');
-      res.redirect('/');
+      passport.authenticate('local', {
+        successRedirect: '/',
+        successFlash: 'User created. You are logged in.'
+      })(req, res);
     } else {
       console.log('user with that email already exists');
       res.redirect('/auth/signup');
@@ -29,6 +32,16 @@ router.post('/signup', function(req,res) {
 
 router.get('/login', function(req, res) {
   res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login'
+}));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
